79decfa0feca7724e50e60e4ed8c8a432d20b5f5
Mouatez Billah <81829138+michou230@users.noreply.github.com>
2026-04-16 09:00:13 +0100

    tested Blade and his edge cases

--- tests/test.py
+++ tests/test.py
@@ -123,7 +123,7 @@ class AI(Character):
             self.ultimate(opponent)
         
 
-mydei = Mydei()
+mydei = Blade()
 char2 = AI("phainon","swipe","wrath","clap",20000)
 
 #MAIN LOOP FOR THE GAME
