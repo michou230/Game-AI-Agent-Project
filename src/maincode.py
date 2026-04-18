c29610f36624ae444eff101ef5804e37dd2e81ff
Mouatez Billah <81829138+michou230@users.noreply.github.com>
2026-04-18 22:33:15 +0100

    removed unnecessary testing material, updates the output and added character selector menu

--- src/maincode.py
+++ src/maincode.py
@@ -1,180 +1,115 @@
-import random
+
 from Characters import *
+from enemies import *
+import time
 
-#Main characters class
-class Character:
-    #initialization
-    def __init__(self,name = "Character",na = "Normal Attack",skill= "Skill",ultimate= "Ultimate",hp = 100):
-        self.name = name
-        self.skill_name = skill
-        self.ultimate_name = ultimate
-        self.na_name = na
-        self.hp = hp
-        self.fullhp = hp
-        self.skill_points = 2
-        self.energy = 0
-        pass
-
-    #Normal Attack method
-    def normalAttack(self,ai):
-        ai.hp -= 10
-        self.skill_points += 1
-        self.energy += 15
-        if ai.hp >= 0:
-            print(f"{self.name} unleashed their Normal Attack: {self.na_name} and caused 10 dmg! The opponents hp is now: {ai.hp}")
-        else: print(f"{self.name} unleashed their Normal Attack: {self.na_name} and caused 10 dmg! The opponents hp is now: 0")
-        return 1
-    #Skill method
-    def skill(self,ai):
-        if self.skill_points > 0:
-            ai.hp -= 15
-            self.skill_points -= 1
-            self.energy += 30
-            if ai.hp >= 0:
-                print(f"{self.name} unleashed their Skill: {self.skill_name} and caused 15 dmg! The opponenets hp is now: {ai.hp}")
-            else: print(f"{self.name} unleashed their Skill: {self.skill_name} and caused 15 dmg! The opponents hp is now: 0")
-            return 1
-        else:
-            print("Not enough skill points to unleash Skill! please choose a different move: ")
-            return 0
-    #Ultimate method
-    def ultimate(self,ai):
-        if self.energy >= 100:
-            ai.hp -= 40
-            self.energy -= 100
-            if ai.hp >= 0:
-                print(f"{self.name} unleashed their Ultimate: {self.ultimate_name} and caused 40 dmg! The opponents hp is now: {ai.hp}")
-            else: print(f"{self.name} unleashed their Ultimate: {self.ultimate_name} and caused 40 dmg! The opponents hp is now: 0")
-            return 1
-        else:
-            print("Not enough energy to unleash Ultimate! please choose a different move: ")
-            return 0
-        
-   
-
-#AI class inherits from Character class
-class AI(Character):
-    def __init__(self, na = "Normal Attack", name = "Character",skill= "Skill",ultimate= "Ultimate",hp = 100, atk = 1000):
-        super().__init__(name,skill,ultimate,na,hp)
-        self.atk = atk
-        self.poisoned = 0
-        self.poisoned_dmg = self.fullhp*0.05
-        pass
-
-    def Poison(self):
-        if self.poisoned == 0:
-            self.poisoned == 3
-            print(f"You Poisoned the opponent and will take DOT for 3 turns, will lose {self.poisoned_dmg} each turn ")
-        else: 
-            self.poisoned -= 1
-            self.hp -= self.poisoned_dmg
-            print(f"Poison effect! The opponent lost {self.poisoned_dmg} hp")
-        
-    #Normal Aattck class: changed text output
-    def normalAttack(self,opp):
-        if opp.name == "Mydei":
-            opp.hp -= self.atk*0.2
-        else:
-            opp.takeDamage(self.atk*0.2,self)
-        self.skill_points += 1
-        self.energy += 15
-        if opp.hp >= 0:
-            print(f"{self.name} unleashed their Normal Attack: {self.na_name} and caused 10 dmg! Your hp is now: {opp.hp}")
-        else: 
-            print(f"{self.name} unleashed their Normal Attack: {self.na_name} and caused 10 dmg! Your hp is now: 0")
-            opp.takeDamage(300)
-        
-    #Skill class: changed text output
-    def skill(self,opp):
-        if self.skill_points > 0:
-            if opp.name == "Mydei":
-                opp.hp -= self.atk * 0.6
-            else:
-                opp.takeDamage(self.atk * 0.6,self)
-            self.skill_points -= 1
-            self.energy += 30
-            if opp.hp >= 0:
-                print(f"{self.name} unleashed their Skill: {self.skill_name} and caused 15 dmg! Your hp is now: {opp.hp}")
-            else: 
-                print(f"{self.name} unleashed their Skill:  {self.skill_name} and caused 15 dmg! Your hp is now: 0")
-                opp.takeDamage(300)
-            
-    #Ultimate class: changed text output
-    def ultimate(self,opp):
-        if self.energy >= 100:
-            if opp.name == "Mydei":
-                opp.hp -= 300
-            else:
-                opp.takeDamage(300,self)
-            self.energy -= 100
-            if opp.hp >= 0:
-                print(f"{self.name} unleashed their Ultimate: {self.ultimate_name} and caused 40 dmg! Your hp is now: {opp.hp}")
-            else: 
-                print(f"{self.name} unleashed their Ultimate: {self.ultimate_name} and caused 40 dmg! Your hp is now: 0")
-                opp.takeDamage(300)
-
-    #AI random move based on available resources
-    def ai_move(self,opponent):
-        available = []
-        proba = []
-
-        available.append("normal")
-        proba.append(30)
-
-        if self.skill_points > 0:
-            available.append("skill")
-            proba.append(60)
-        if self.energy >= 100:
-            available.append("ultimate")
-            proba.append(70)
-        if "ultimate" in available and self.hp <= self.fullhp*0.4:
-            proba[-1] += 30
-        
-        move = random.choices(available,proba)[0]
-
-        if move == "normal":
-            self.normalAttack(opponent)
-        elif move == "skill":
-            self.skill(opponent)
-        elif move == "ultimate":
-            self.ultimate(opponent)
-        
-
-mydei = Hyacine()
-char2 = AI("phainon","swipe","wrath","clap",20000)
+char2 = Lieutenant()
 
 #MAIN LOOP FOR THE GAME
 
-"""pick = 0
-while pick == 0:
-    player = int(input("Please pick character corresponding to the numbers:\n1.Phainon\n2.Mydei\n3.Sparxie\n4.The Herta\n5.Dr. Ratio\n6.Acheron\n7.Jungliu\n8.Blade\n9.Kafka\n10.Himeko\n0.Random\nChoose a number: "))
-    """
-while mydei.hp > 0 and char2.hp > 0:
+p = 0
+c = 0
+tab = ["Mydei","Sparxie","Hyacine","Blade","Kafka"]
+while p == 0:
+    while c == 0:
+        try:
+            player = int(input("Please pick character corresponding to the numbers:\n[0].Random | [1].Mydei | [2].Sparxie | [3].Blade | [4].Kafka | [5].Hyacine\nChoose a number: "))
+            c = 1
+        except ValueError:
+            print("please pick a number.")
+    while player not in [0,1,2,3,4,5]:
+        try:
+            player = int(input("Please pick a valid number: "))
+        except ValueError:
+            print("A number please.")
+    match(player):
+        case 0:
+            char = random.choice(tab)
+            match(char):
+                case "Mydei":
+                    p1 = Mydei()
+                    print(f"Picked the Character {p1.name}")
+                    p = 1
+                case "Kafka":
+                    p1 = Kafka()
+                    print(f"Picked the Character {p1.name}")
+                    p = 1
+                case "Hyacine":
+                    p1 = Hyacine()
+                    print(f"Picked the Character {p1.name}")
+                    p = 1
+                case "Sparxie":
+                    p1 = Sparxie()
+                    print(f"Picked the Character {p1.name}")
+                    p = 1
+                case "Blade":
+                    p1 = Blade()
+                    print(f"Picked the Character {p1.name}")
+                    p = 1
+        case 1:
+            p1 = Mydei()
+            print(f"Picked the Character {p1.name}")
+            p = 1
+        case 2:
+            p1 = Sparxie()
+            print(f"Picked the Character {p1.name}")
+            p = 1
+        case 3:
+            p1 = Blade()
+            print(f"Picked the Character {p1.name}")
+            p = 1
+        case 4:
+            p1 = Kafka()
+            print(f"Picked the Character {p1.name}")
+            p = 1
+        case 5:
+            p1 = Hyacine()
+            print(f"Picked the Character {p1.name}")
+            p = 1
+
+    
+print("\nRemember, your moves are:\n[1] for Normal Attack\n[2] for Skill\n[3] for Ultimate")
+turn = 1
+while p1.hp > 0 and char2.hp > 0:
     player_move = 0
+    print("\t"+ 30*"=" + f"TURN {turn}" + 30*"=" + "\n")
     while player_move == 0:
-        if mydei.poisoned >= 1:
-            mydei.Poison()
-        print("please select your move:\n1 for normal attack\n2 for skill\n3 for ultimate")
-        mydei.displayInfo(char2)
-        move = int(input(f"You choose: "))
+        if p1.poisoned >= 1:
+            p1.Poison()
+        turn += 1
+        p1.displayInfo()
+        char2.displayInfo()
+        m = 0
+        while m == 0:
+            try:
+                move = int(input(f"\nYou choose: "))
+                if move not in [1,2,3]:
+                    print("Invalid move, try again.")
+                else: m = 1
+            except ValueError:
+                print("Only numbers please.")
+        print("\n")
         if move == 1:
-            player_move = mydei.normalAttack(char2)
+            player_move = p1.normalAttack(char2)
         elif move == 2:
-            player_move = mydei.skill(char2)
+            player_move = p1.skill(char2)
         elif move == 3 :
-            player_move = mydei.ultimate(char2)
-        else : print("Invalid move, try again.")
+            player_move = p1.ultimate(char2) 
     
     if player_move == 1 and char2.hp > 0:
         if char2.poisoned >= 1:
             char2.Poison()
-        char2.ai_move(mydei)
+        print("\nOpponent thinking. . .\n")
+        time.sleep(2)
+        char2.ai_move(p1)
+        print("\n")
+        time.sleep(2)
 
-    if mydei.hp <= 0:
-        print(f"Game Over! {char2.name} won! player {mydei.name} lost!")
+    if p1.hp <= 0:
+        print(f"Game Over! {char2.name} won! player {p1.name} lost!")
         break
     elif char2.hp <= 0:
-        print(f"Game Over! {mydei.name} won! player {char2.name} lost!")
+        print(f"Game Over! {p1.name} won! player {char2.name} lost!")
         break
 
 
