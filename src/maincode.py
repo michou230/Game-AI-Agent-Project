221d1b8dd6b226d9b6b390cded9bc0c7987c0788
Mouatez Billah <81829138+michou230@users.noreply.github.com>
2026-04-20 21:00:31 +0100

    added enemy selector

--- src/maincode.py
+++ src/maincode.py
@@ -3,13 +3,17 @@ from Characters import *
 from enemies import *
 import time
 
-char2 = Sunday()
+
 
 #MAIN LOOP FOR THE GAME
 
 p = 0
 c = 0
+
+pick = 0
+choice = 0
 tab = ["Mydei","Sparxie","Hyacine","Blade","Kafka"]
+tab2 = ["Pollux","Lieutenant","Sunday"]
 while p == 0:
     while c == 0:
         try:
@@ -68,9 +72,51 @@ while p == 0:
             p = 1
 
     
+while pick == 0:
+    while choice == 0:
+        try:
+            player = int(input("\nPlease pick the enemy corresponding to the numbers:\n[0].Random | [1].Pollux | [2].Silvermane Lieutenant | [3].Harmonic Choir\nChoose a number: "))
+            choice = 1
+        except ValueError:
+            print("please pick a number.")
+    while player not in [0,1,2,3]:
+        try:
+            player = int(input("Please pick a valid number: "))
+        except ValueError:
+            print("A number please.")
+    match(player):
+        case 0:
+            char = random.choice(tab2)
+            match(char):
+                case "Pollux":
+                    p2 = Pollux()
+                    print(f"Picked the enemy {p2.name}")
+                    pick = 1
+                case "Lieutenant":
+                    p2 = Lieutenant()
+                    print(f"Picked the enmey {p2.name}")
+                    pick = 1
+                case "Sunday":
+                    p2 = Sunday()
+                    print(f"Picked the enemy {p2.name}")
+                    pick = 1
+        case 1:
+            p2 = Pollux()
+            print(f"Picked the enemy {p2.name}")
+            pick = 1
+        case 2:
+            p2 = Lieutenant()
+            print(f"Picked the enmey {p2.name}")
+            pick = 1
+        case 3:
+            p2 = Sunday()
+            print(f"Picked the enemy {p2.name}")
+            pick = 1
+
+    
 print("\nRemember, your moves are:\n[1] for Normal Attack\n[2] for Skill\n[3] for Ultimate")
 turn = 1
-while p1.hp > 0 and char2.hp > 0:
+while p1.hp > 0 and p2.hp > 0:
     player_move = 0
     print("\t"+ 30*"=" + f"TURN {turn}" + 30*"=" + "\n")
     while player_move == 0:
@@ -78,7 +124,7 @@ while p1.hp > 0 and char2.hp > 0:
             p1.Poison()
         turn += 1
         p1.displayInfo()
-        char2.displayInfo()
+        p2.displayInfo()
         m = 0
         while m == 0:
             try:
@@ -90,26 +136,26 @@ while p1.hp > 0 and char2.hp > 0:
                 print("Only numbers please.")
         print("\n")
         if move == 1:
-            player_move = p1.normalAttack(char2)
+            player_move = p1.normalAttack(p2)
         elif move == 2:
-            player_move = p1.skill(char2)
+            player_move = p1.skill(p2)
         elif move == 3 :
-            player_move = p1.ultimate(char2) 
+            player_move = p1.ultimate(p2) 
     
-    if player_move == 1 and char2.hp > 0:
-        if char2.poisoned >= 1:
-            char2.Poison()
+    if player_move == 1 and p2.hp > 0:
+        if p2.poisoned >= 1:
+            p2.Poison()
         print("\nOpponent thinking. . .\n")
         time.sleep(2)
-        char2.ai_move(p1)
+        p2.ai_move(p1)
         print("\n")
         time.sleep(2)
 
     if p1.hp <= 0:
-        print(f"Game Over! {char2.name} won! player {p1.name} lost!")
+        print(f"Game Over! {p2.name} won! player {p1.name} lost!")
         break
-    elif char2.hp <= 0:
-        print(f"Game Over! {p1.name} won! opponent {char2.name} lost!")
+    elif p2.hp <= 0:
+        print(f"Game Over! {p1.name} won! opponent {p2.name} lost!")
         break
 
 
