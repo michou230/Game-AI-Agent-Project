58ce34e108fd1f2c83816c262f5329d96b347ee6
Mouatez Billah <81829138+michou230@users.noreply.github.com>
2026-05-11 16:27:58 +0100

    added osts + tested them using pygame

--- tests/test.py
+++ tests/test.py
@@ -4,25 +4,15 @@ import time
 from pathlib import Path
 from playsound import playsound
 
+import pygame
+import time
 base = Path(__file__).resolve().parent
-audio = base.parent / "data" / "assets" / "AudioCutter_sundayyy.mp3"
+pygame.mixer.init()
+planarcadia = base.parent / "data" / "assets" / "planarcadia.mp3"
+pygame.mixer.music.load(str(planarcadia))
+pygame.mixer.music.set_volume(0.3)
 
-def slow(text):
-    for c in text:
-        sys.stdout.write(c)
-        sys.stdout.flush()
-        time.sleep(0.05)
-    print()
+pygame.mixer.music.play(-1)
 
-
-playsound(str(audio), block = False)
-time.sleep(2.7)
-slow("\nAll the work of creation has been completed..")
-time.sleep(1.97)
-slow("The inevitable day has arrived..")
-time.sleep(1.90)
-slow("The Embryo of Philosophy..")
-time.sleep(1.4)
-slow("WILL RESHAPE FOR US ALL OF REALITY!\n")
-time.sleep(4)
-print("The boss has transformed to its second phase!\n")
+while True:
+    time.sleep(1)
